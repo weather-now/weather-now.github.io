111ee7d2f1865a7357147b7d93ebe277d96b7653
ryan.lkm <ryan.lkm@kakaocorp.com>
2020-08-07 16:53:34 +0900

    아이콘 변경

--- build-and-get-webapp.sh
+++ build-and-get-webapp.sh
@@ -1,4 +1,7 @@
 . set-webapp-path.sh
 rm -rf $WEBAPP_PATH/dist
 npm run build --prefix $WEBAPP_PATH
+git rm -rf css js
+rm -rf css js
 cp -R ~/WebstormProjects/aws-on-leaflet/dist/* .
+git add css js
